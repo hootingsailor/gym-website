33a34c5718961616f7226b0e9be76cd9a3e5d82b
Aakash Sharma <sharmaaakash824@gmail.com>
2022-06-09 11:38:49 +0530

    minor  update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Home.css';
+import banner from "../img/5.jpg";
 
 function Home() {
   return (
     <div className='main'>
-      <h1>My Gym Website</h1>
+      <div className="banner">
+        <img src={banner} alt="" />
+        <h1>I hated every minute of training, but I said, don’t quit. Suffer now and live the rest of your life as a champion. </h1>
+      </div>
+      
     </div>
   )
 }
