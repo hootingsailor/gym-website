817d6e9f1adcaf868d1fba431d72d21423782ce9
Aakash Sharma <sharmaaakash824@gmail.com>
2022-06-13 15:49:47 +0530

    working on banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
+import Banner from './Banner';
 import './Home.css';
-import banner from "../img/5.jpg";
 
 function Home() {
   return (
     <div className='main'>
-      <div className="banner">
-        <img src={banner} alt="" />
-        <h1>I hated every minute of training, but I said, don’t quit. Suffer now and live the rest of your life as a champion. </h1>
-      </div>
-      
+      <Banner />
     </div>
   )
 }
